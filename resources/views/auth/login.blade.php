a320d5a2968a13d635b30522394221da51f1dc66
Alexandr Marchenko <alexmarch232@users.noreply.github.com>
2016-04-30 01:28:16 +0300

    Added files via upload

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,9 @@
+@extends('layout')
+
+@section('content')
+<div>
+    <h2>Sign In</h2>
+
+</div>
+
+    @stop
---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 27.04.16
+ * Time: 3:56
+ */
